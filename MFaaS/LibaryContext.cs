a81f5f4a104264c2a7e806aecdf178f4f49f2c5f
Arne <arnestittrich@gmail.com>
2022-03-08 01:48:55 +0100

    Projektdateien hinzufügen.

---
+++ MFaaS/LibaryContext.cs
@@ -0,0 +1,34 @@
+﻿using MFaaS.Models;
+using Newtonsoft.Json;
+
+namespace MFaaS
+{
+    public class LibaryContext
+    {
+        private readonly string _QutationsPath = AppDomain.CurrentDomain.BaseDirectory + "Ressources/quotes.json";
+        private static LibaryContext _Instance = null;
+
+        public List<Quotation> Quotations { get; private set; }
+        
+        private LibaryContext()
+        {
+            string quotationsJson = File.ReadAllText(_QutationsPath);
+            if (string.IsNullOrWhiteSpace(quotationsJson))
+                Quotations = new List<Quotation>();
+            else
+                Quotations = JsonConvert.DeserializeObject<List<Quotation>>(quotationsJson);
+        }
+
+        public static LibaryContext Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new LibaryContext();
+                }
+                return _Instance;
+            }
+        }
+    }
+}
---
+++ MFaaS/Models/Quotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace MFaaS.Models
+{
+    [DataContract]
+    public class Quotation
+    {
+        [DataMember(Name = "quote")]
+        public string Quote { get; set; }
+        [DataMember(Name = "actor")]
+        public string Actor { get; set; }
+    }
+}
