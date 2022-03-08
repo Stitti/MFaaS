f00d828c28688c27a9d015e04c0915ab326ece15
Arne <arnestittrich@gmail.com>
2022-03-08 04:31:24 +0100

    Added actors

---
+++ MFaaS/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace MFaaS.Models
+{
+    [DataContract]
+    public class Actor
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+    }
+}
